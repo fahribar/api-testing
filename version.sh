08109480c7d5128696f4f62c3f8ff362be42a079
Fakhri Akbar Pratama <fakhri.akbar.pratama@corp.bri.co.id>
2022-07-15 10:50:09 +0700

    versioning new

--- version.sh
+++ version.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-cat ./version.txt > ./rollback.txt
+version="0.$(./git-buildnumber|cut -d '.' -f2-4)"
+#version=$(cat ./version.txt)
+echo $version > ./version.txt
+HELM_FOLDER=helm-chart
+HELM_VERSION="0.$(./git-buildnumber|cut -d '.' -f2-3)"
+sed -i '18s/.*/version: '${HELM_VERSION}'/' ${HELM_FOLDER}/Chart.yaml
+sed -i '24s/.*/appVersion: '${HELM_VERSION}'/' ${HELM_FOLDER}/Chart.yaml
+sed -i '13s/.*/  tag: "'${version}'"/' ${HELM_FOLDER}/values.yaml
+sed -i '13s/.*/  tag: "'${version}'"/' ${HELM_FOLDER}/development.values.yaml
